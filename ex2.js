31b5e31cba3011772eb0f2e6d5c376bee495a967
caioonofre <protectcaio@gmail.com>
2025-09-08 21:59:04 -0300

    exercicio 2 pela metade

---
+++ ex2.js
@@ -0,0 +1,21 @@
+let num1 = 35
+let num2 = 20
+
+console.log(num1, "-", num2, " = ", num1-num2)
+console.log(num1, "+", num2, " = ", num1+num2)
+console.log(num1, "*", num2, " = ", num1*num2)
+console.log(num1, "/", num2, " = ", num1/num2)
+
+if (num1>num1) {
+    console.log(num1, "é maior que ", num2)
+}
+else {
+    console.log(num1, "é menor que ", num2)
+}
+
+if (num1===num1) {
+    console.log(num1, "é igual a ", num2)
+}
+else {
+    console.log(num1, "não é igual a ", num2)
+}
