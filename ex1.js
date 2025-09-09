dc892e4b0fb36de76f569b7113cac6586fadf8a6
Alicite <lunacarter1503@gmail.com>
2025-09-08 21:51:04 -0300

    ex1 iniciado

---
+++ ex1.js
@@ -0,0 +1,13 @@
+// Nome: [Seu Nome] (string)
+// Ano de Nascimento: [Seu Ano] (number)
+// Idade: [Sua Idade] (number)
+// É Estudante: [true/false] (boolean)]
+
+let nome = 'Leticia de Paula'
+const anoNascimento = 1995
+let idade = 2025 - anoNascimento
+let isStudent = true
+
+console.log("Nome: " + nome + " (" + typeof(nome) + ")")
+console.log("Nome:", nome, "(", typeof(nome), ")")
+console.log(`Nome: ${nome} (${typeof(nome)})`)
