f16a17da3131e832e4b783866e6ad54f5a48eaa3
Idenilson27 <voigtidenilson@gmail.com>
2025-09-09 19:28:19 -0300

    exercicio feito 01

--- ex1.js
+++ ex1.js
@@ -1,2 +1,9 @@
 let nome = "Idenilson";
-console.log("Meu nome é " + nome);
+const anoNascimento = 1986;
+let idade = 2025 - anoNascimento;
+let isEstudante = false; 
+
+console.log(`Nome: ${nome} (${typeof nome})`);
+console.log(`Ano de Nascimento: ${anoNascimento} (${typeof anoNascimento})`);
+console.log(`Idade: ${idade} (${typeof idade})`);
+console.log(`É Estudante: ${isEstudante} (${typeof isEstudante})`);
