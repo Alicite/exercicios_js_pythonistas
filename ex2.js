d66714b7aa96d0b496781fabdef463f9fa3de437
nicolaslb22 <nicolaslanger22bett@gmail.com>
2025-09-09 20:30:34 -0300

    exercicio1

---
+++ ex2.js
@@ -0,0 +1,35 @@
+// ## Exercício 2: Operadores e Condicionais
+
+// **Objetivo:** Utilizar operadores aritméticos, de comparação e lógicos, e estruturas condicionais (`if/else if/else`).
+
+// 1. Crie duas variáveis `num1` e `num2` com valores numéricos de sua escolha.
+
+// 1. Realize as quatro operações aritméticas básicas (`+`, `-`, `*`, `/`) entre `num1` e `num2` e imprima os resultados.
+
+// 1. Verifique se `num1` é maior que `num2` e imprima o resultado.
+
+// 1. Verifique se `num1` é igual a `num2` usando o operador de igualdade estrita (`===`) e imprima o resultado.
+
+// 1. Crie uma variável `nota` e atribua a ela um valor entre 0 e 100.
+
+// 1. Usando `if/else if/else`, imprima a classificação da nota:
+//   - 90-100: "A"
+//   - 80-89: "B"
+//   - 70-79: "C"
+//   - 60-69: "D"
+//   - Abaixo de 60: "F"
+
+// 1. Crie duas variáveis `num1` e `num2` com valores numéricos de sua escolha.
+
+let num1 = 2;
+let num2 = 2;
+
+// 2. Realize as quatro operações aritméticas básicas (`+`, `-`, `*`, `/`) entre `num1` e `num2` e imprima os resultados.
+
+console.log ("soma:", num1 + num2);
+console.log ("subtração", num1 - num2);
+console.log ("multiplicação", num1 * num2);
+console.log ("divisão", num1 / num2);
+
+
+
