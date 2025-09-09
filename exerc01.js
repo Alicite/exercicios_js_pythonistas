728eef51252542437645db01519b60faefd30940
Walmir1974 <walmir.vmartins@gmail.com>
2025-09-09 20:30:25 -0300

    exerc 01 feito

--- exerc01.js
+++ exerc01.js
@@ -1,7 +1,7 @@
-let nome = "Walmir Vicente"
-const anoNascimento = 1974
-let idade = 51
-let isEstudante = false;
+let nome = "Walmir Vicente";
+const anoNascimento = 1974;
+let idade = 2025 - anoNascimento;
+let isEstudante = false; 
 
 console.log(`Nome:   ${nome} (${typeof nome})`);
 console.log(`Ano de Nascimento:  ${anoNascimento} (${typeof anoNascimento})`);
