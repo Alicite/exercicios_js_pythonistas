104acdbbc885e4005c8affe15bc8dc9df2b7336b
Jeovana <jeovana.klein123@gmail.com>
2025-09-09 20:21:13 -0300

    Exercício 3 Finalizado

--- ex3.js
+++ ex3.js
@@ -5,10 +5,27 @@ for (let i = 1; i <= 10; i++) {
 
 
 
-
 let contador = 0;
 
 while (contador < 20) {
     console.log(contador);
     contador += 2;
+}
+
+
+
+const nomes = ['Fernando', 'Jeovana', 'Luiz', 'Yasmim'];
+
+for (const nome of nomes) {
+    console.log(nome);
+}
+
+
+const pessoa = {
+"nome": "Carlos",
+"idade": 28,
+"cidade": "Rio Grande do Sul"
+}
+for (const chave in pessoa) {
+    console.log(chave + ":", pessoa[chave]); ;
 }
