6f2917dcc40c92e7e4c283e2787e5dce26bcab5d
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-09-08 22:00:04 -0300

    exercício-1-e-começo-do-2

---
+++ ex1.js
@@ -0,0 +1,9 @@
+const nome = "Miguel"
+const anoNascimento = (2008)
+let idade = (2025 - anoNascimento)
+isEstudante = true
+
+console.log(`Nome: ${nome}`)
+console.log(`Ano de Nascimento: ${anoNascimento}`)
+console.log(`Sua idade: ${idade}`)
+console.log(`É Estudante: ${isEstudante}`)
