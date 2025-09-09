5757105478b9a5a8c4bfd70629393ba641cf9afb
jeoklain <jeovana.klein123@gmail.com>
2025-09-08 21:27:28 -0300

    Realização do exercício 1

---
+++ ex1.js
@@ -0,0 +1,10 @@
+let nome = "Jeovana"
+const anoNascimento = 2007;
+let anoAtual = 2025;
+let idade = anoAtual- anoNascimento;
+let isestudante = true;
+
+console.log("Nome:", nome, "(", typeof nome, ")");
+console.log("Ano de Nascimento:", anoNascimento, "(", typeof anoNascimento, ")");
+console.log("Idade:", idade, "(", typeof idade, ")");
+console.log("É estudante?", isestudante, "(", typeof isestudante, ")");
