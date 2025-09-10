e624071aeea661fa2d125ec9a8a83bc7c4f95aa2
Jeovana <jeovana.klein123@gmail.com>
2025-09-09 21:17:25 -0300

    Arrumando Estrutura Exercício 2

--- ex2.js
+++ ex2.js
@@ -20,19 +20,14 @@ console.log("num1 === num2:", num1 === num2)
 
 
 if (nota >= 90 && nota <= 100) {
-    console.log("A");} 
-    
-    else if (nota >= 80 && nota <= 89) {
-    console.log("B");} 
-
-    else if (nota >= 70 && nota <= 79) {
-    console.log("C");}
-
-    else if (nota >= 60 && nota <= 69) {
-    console.log("D");} 
-    
-    else if (nota < 60 && nota >= 0) {
-    console.log("F");} 
-    
-    else {
+    console.log("A");
+} else if (nota >= 80 && nota <= 89) {
+    console.log("B");
+} else if (nota >= 70 && nota <= 79) {
+    console.log("C");
+} else if (nota >= 60 && nota <= 69) {
+    console.log("D");
+} else if (nota < 60 && nota >= 0) {
+    console.log("F");
+} else {
     console.log("Nota inválida");}
