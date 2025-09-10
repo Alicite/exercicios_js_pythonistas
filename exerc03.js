386528979fed6fe143c58726a186879ad3c4b235
Walmir1974 <walmir.vmartins@gmail.com>
2025-09-09 21:40:12 -0300

    exerc03 feito

---
+++ exerc03.js
@@ -0,0 +1,12 @@
+for(let num = 1;num <= 10;num+=1) {
+    console.log(num)
+}
+
+let numero = 0
+while (numero < 21) {
+    if (numero % 2 == 0) {
+        console.log(numero)
+    }
+    numero += 1
+}
+
