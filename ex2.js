f8c7a500141f68a859997aaefb10117e74679073
nicolaslb22 <nicolaslanger22bett@gmail.com>
2025-09-09 21:24:59 -0300

    exer.2 modificações

--- ex2.js
+++ ex2.js
@@ -19,31 +19,31 @@
 //   - 60-69: "D"
 //   - Abaixo de 60: "F"
 
-// 1. Crie duas variáveis `num1` e `num2` com valores numéricos de sua escolha.
+// 1.
 
 let num1 = 2;
 let num2 = 4;
 
-// 2. Realize as quatro operações aritméticas básicas (`+`, `-`, `*`, `/`) entre `num1` e `num2` e imprima os resultados.
+// 2. 
 
 console.log ("soma:", num1 + num2);
 console.log ("subtração:", num1 - num2);
 console.log ("multiplicação:", num1 * num2);
 console.log ("divisão:", num1 / num2);
 
-// 3. Verifique se `num1` é maior que `num2` e imprima o resultado.
+// 3. 
 
 console.log("Verificar se num1 é maior que num2:",num1 > num2);
 
-// 4. Verifique se `num1` é igual a `num2` usando o operador de igualdade estrita (`===`) e imprima o resultado.
+// 4. 
 
 console.log("Verificar igualdade:", num1 === num2);
 
-// 5. Crie uma variável `nota` e atribua a ela um valor entre 0 e 100.
+// 5. 
 
 let nota = 100
 
-// 6. Usando `if/else if/else`, imprima a classificação da nota:
+// 6. 
 //   - 90-100: "A"
 //   - 80-89: "B"
 //   - 70-79: "C"
---
+++ ex3.js
@@ -0,0 +1,15 @@
+// ## Exercício 3: Loops
+
+// **Objetivo:** Praticar o uso de loops `for` e `while`.
+
+// 1. Use um loop `for` para imprimir os números de 1 a 10.
+
+// 2. Use um loop `while` para imprimir os números pares de 0 a 20.
+
+// 3. Crie um array de nomes. Use um loop `for...of` para imprimir cada nome no console.
+
+// 4. Crie um objeto com algumas propriedades (nome, idade, cidade). Use um loop `for...in` para imprimir cada chave e seu respectivo valor.
+
+
+// 1.
+
