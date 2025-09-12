f9c4da4a2d11757e3de418c9497e17f549616478
Caio <4552233750@cedup.local>
2025-09-11 21:57:37 -0300

    add: exercicio 4 resolvido

--- ex4.js
+++ ex4.js
@@ -1,25 +1,55 @@
 function somar(num1, num2) {
-    console.log(`${num1} + ${num2}  =  ${num1+num2}`)
+    return `${num1} + ${num2}  =  ${num1+num2}`
 }
 console.log("")
 console.log("Função somar")
-somar(1, 1)
+console.log(somar(1, 1))
 
-function saudacao(nome) {
-    console.log(`Olá ${nome}`)
+function saudacao(nome = "visitante") {
+    return `Olá ${nome}`
 }
 console.log("")
 console.log("Função saudação")
-saudacao("Caio")
+console.log(saudacao("Caio"))
 
 function ehPar(number) {
     if (number%2 === 0) {
-        console.log(`O número ${number} é Par`)
+        return `O número ${number} é Par`
     } else {
-        console.log(`O número ${number} é Impar`)
+        return `O número ${number} é Impar`
     }
 }
 
 console.log("")
 console.log("Função ehpar")
-ehPar(8)
+console.log(ehPar(8))
+
+function aplicarOperacao(callback, n1, n2){
+    return callback(n1, n2)
+}
+
+function subitrair(num1, num2){
+    return `${num1} - ${um2} = ${num1-num2}`
+}
+
+function dividir(num1, num2){
+    if(num2==0){
+        return "Impossivel dividir por 0"
+    } else {
+        return `${num1} / ${um2} = ${num1/num2}`
+    } 
+    
+}
+
+function multiplicar(num1, num2){
+    return `${num1} * ${um2} = ${num1*num2}`
+}
+
+op = [somar, subitrair, multiplicar, dividir]
+for (f in op){
+    console.log(aplicarOperacao(f, 5, 5))
+}
+console.log(aplicarOperacao(somar, 5, 5))
+console.log(aplicarOperacao(subitrair, 5, 5))
+console.log(aplicarOperacao(multiplicar, 5, 5))
+console.log(aplicarOperacao(dividir, 5, 5))
