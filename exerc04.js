c97c8bfce2c57b878acf2d9ad1cfca2f3e513a1d
Walmir1974 <walmir.vmartins@gmail.com>
2025-09-11 21:59:58 -0300

    exer 04 executado

--- exerc04.js
+++ exerc04.js
@@ -1,8 +1,45 @@
-function somar(a, b) {
+function somar(a1, b1) {
 
-    return a + b;
+    return a1 + b1;
+}
+console.log(2,3)
+
+
+const saudacao = function(nome="Darthvader"){
+    return `Olá, ${nome}`
+    
 }
 
+const ehPar = a1 => a1 % 2 === 0 ? true : false
+console.log(ehPar(21))
+console.log(ehPar(30))
+console.log(ehPar(-52))
+
+function aplicarOperacao(callback, a1, b1){
+    return callback(a1, b1)
+}
+
+function subtrair(a1, b1){
+    return a1 - b1
+}
+
+function multiplicar(a1 , b1){
+    return a1 * b1
+}
+
+function dividir(a1, b1){
+    if(b1 === 0){
+        return "Impossivel dividir por 0!"
+    }else {
+            return a1 / b1
+        }
+    }
 
 
-
+op = [somar, subtrair, multiplicar.dividir]
+for (f in op) {
+    console.log(aplicarOperacao(somar, 5, 2))
+    console.log(aplicarOperacao(subtrair, 5, 2))
+    console.log(aplicarOperacao(multiplicar, 5, 2))
+    console.log(aplicarOperacao(dividir, 5, 2))
+}
