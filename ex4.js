046b73a5ce978e37f7509f23703d48b223d2e66a
Jeovana <jeovana.klein123@gmail.com>
2025-09-11 21:53:35 -0300

    Alterando e Trabalhando com o Callback

--- ex4.js
+++ ex4.js
@@ -11,16 +11,38 @@ function ehpar(num) {
     return num % 2 === 0;
 }
 
-function aplicarOperacao(operacao, a, b) {
-return operacao(a, b);
+function aplicarOperacao(callback, a, b) {
+return callback(a, b);
 }
 
+function subtrair (num1, num2) {
+    return num1 - num2;
+}
 
+function multiplicar (num1, num2) {
+    return num1 * num2;
+}   
+
+function dividir (num1, num2) {
+    if (num2 === 0) {
+        return "Erro: Divisão por zero não é permitida.";
+    }
+    else {
+        return num1 / num2;
+    }
+}
+
+    op [somar, subtrair, multiplicar, dividir]
+    for (f in op){
+        console.log(aplicarOperacao(f, 10, 2));
+    }
+ 
 
 
 
 console.log(somar(5, 3));
 console.log(saudacao("Jeovana")); 
-console.log(ehpar(4)); 
-console.log(ehpar(7)); 
-console.log(aplicarOperacao(somar, 5, 20));
+console.log(ehpar(21)); 
+console.log(ehpar(231)); 
+console.log(ehpar(-42));
+
