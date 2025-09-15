64a7a93f5b52edb43528dc7ae6bbb0d9120ba1a2
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-09-15 23:51:07 +0000

    feito o exercicio 2

--- ex2.js
+++ ex2.js
@@ -1,3 +1,47 @@
-num1 = 90
-num2 = 31
+num1 = 9
+num2 = 3
 
+nota = 90
+
+mais = (num1 + num2)
+console.log(`O resultado da soma é: ${mais}`)
+
+menos = (num1 - num2)
+console.log(`O resultado da subtração é: ${menos}`)
+
+multiplicacao = (num1 * num2)
+console.log(`O resultado da multiplicação é: ${multiplicacao}`)
+
+divisao = (num1 / num2)
+console.log(`O resultado da divisão é: ${divisao}`)
+
+if (num1 > num2)
+    console.log("O número 1 é maior")
+else 
+    console.log("O número 2 é menor")
+
+if (num1 === num2)
+    console.log("Os números sao iguais")
+else
+    console.log("Os números são diferetes")
+
+
+if (nota <= 60){
+    console.log("A nota avaliada é considerada um : F")
+}
+
+if ((nota > 60) && (nota <= 69)){
+    console.log("A nota avaliada é considerada um: D")
+}
+
+if ((nota > 69) && (nota <= 79)){
+    console.log("A nota avaliada é considerada um: C")
+}
+    
+if ((nota > 79) && (nota <= 89)){
+    console.log("A nota avaliada é considerada um: B")
+}
+   
+if ((nota > 89) && (nota <= 100)) {
+    console.log("A nota avaliada é considerada um: A")
+}
