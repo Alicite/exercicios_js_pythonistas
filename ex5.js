877ce45f544294731dd78f38652097ab9476156e
nicolaslb22 <nicolaslanger22bett@gmail.com>
2025-09-16 00:45:45 +0000

    exer.5 resolvido

---
+++ ex5.js
@@ -0,0 +1,21 @@
+// ## Exercício 5: Arrays e Objetos
+
+// **Objetivo:** Manipular arrays e objetos, adicionando, removendo e acessando elementos.
+
+// 1. Crie um array chamado `cores` com pelo menos 3 cores.
+
+// 2. Adicione uma nova cor ao final do array.
+
+// 3. Remova a primeira cor do array.
+
+// 4. Imprima o array resultante.
+
+// 5. Crie um objeto `livro` com as propriedades `titulo`, `autor` e `anoPublicacao`.
+
+// 6. Adicione uma nova propriedade `genero` ao objeto `livro`.
+
+// 7. Imprima o `titulo` e o `autor` do livro.
+
+// 8. Use `Object.keys()` e `Object.values()` para imprimir as chaves e os valores do objeto `livro`.
+
+
