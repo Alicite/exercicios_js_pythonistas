dc8dbc680f37156a254d5e59670c740147b1a72c
deBokorni <afonssoleandro@gmail.com>
2025-09-16 20:38:37 -0300

    Exc5 quase pronto

---
+++ exc5.js
@@ -0,0 +1,27 @@
+console.log("----CORES----")
+console.log()
+
+let cores = [`Verde`, `Roxo`, `Azul`]
+
+cores.push(`Amarelo`)
+cores.shift([0])
+console.log(cores)
+console.log()
+console.log()
+
+console.log("----LIVRO----")
+console.log()
+
+let livro = {
+    titulo : "Para Todas as Garotas Que Eu Já Peguei",
+    autor : "Pedro Algusto Rodrigues da Silva",
+    anoPublicacao : 2025
+}
+
+livro.genero = "Conto de Fadas"
+
+console.log(`Título = ${livro.titulo}`)
+console.log(`Gênero = ${livro.genero}`)
+console.log(`Autor = ${livro.autor}`)
+
+
