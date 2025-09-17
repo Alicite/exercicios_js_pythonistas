a5ed614b0cad2155c8de8def36a741829ff7cc58
miguelkik <miguelaparecidoribeiro06@gmail.com>
2025-09-16 21:32:53 -0300

    fazendo ex4

---
+++ ex4.js
@@ -0,0 +1,24 @@
+function soma(num1, num2) {
+  return (`A soma dos dois números é: ${num1 + num2}`);
+}
+console.log(soma(1,4))
+
+///parte 2222
+
+function saudacao(nome) {
+return (`Olá, ${nome}!`);
+}
+console.log(saudacao("Miguel lindo"))
+
+///partew 3333
+function ehpar(num){
+    return (`O número é: ${num % 2 === 0}`)
+}
+console.log(ehpar(52))
+
+//parte 444444
+function aplicarOperacao(operacao){
+    console.log(num11, operacao, num22)
+    
+
+}
