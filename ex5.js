9cb48886caca176e292fe6a93eb00cb599e963ec
Alicite <lunacarter1503@gmail.com>
2025-09-16 21:24:23 -0300

    ex5-3 resolvido

--- ex5.js
+++ ex5.js
@@ -1,14 +1,14 @@
 // Crie um array chamado cores com pelo menos 3 cores.
 const cores = ['azul', 'vermelho', 'verde'];
-console.log(cores);
 
 // Adicione uma nova cor ao final do array.
 cores.push("branco");
 cores.unshift("preto");
-console.log(cores);
 
 // Remova a primeira cor do array.
 cores.shift()
 cores.pop()
+
+// Imprima o array resultante.
 console.log(cores);
 
