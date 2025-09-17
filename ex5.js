52f34e663ce1491d851ea4308f162a502decae61
Alicite <lunacarter1503@gmail.com>
2025-09-16 21:33:43 -0300

    ex5-5 resolvido

--- ex5.js
+++ ex5.js
@@ -1,14 +1,21 @@
-// Crie um array chamado cores com pelo menos 3 cores.
-const cores = ['azul', 'vermelho', 'verde'];
+// // Crie um array chamado cores com pelo menos 3 cores.
+// const cores = ['azul', 'vermelho', 'verde'];
 
-// Adicione uma nova cor ao final do array.
-cores.push("branco");
-cores.unshift("preto");
+// // Adicione uma nova cor ao final do array.
+// cores.push("branco");
+// cores.unshift("preto");
 
-// Remova a primeira cor do array.
-cores.shift()
-cores.pop()
+// // Remova a primeira cor do array.
+// cores.shift()
+// cores.pop()
 
-// Imprima o array resultante.
-console.log(cores);
+// // Imprima o array resultante.
+// console.log(cores);
 
+// Crie um objeto livro com as propriedades titulo, autor e anoPublicacao.
+const livro = {
+    titulo: 'O senhor dos anéis',
+    autor: 'J. R. R. Tolkien',
+    anoPublicacao: 1954
+};
+console.log(livro)
