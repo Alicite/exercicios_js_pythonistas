8864f974a66147660acbc1d762b0b0bbc0f14f36
Jeovana <jeovana.klein123@gmail.com>
2025-09-16 21:05:58 -0300

    Realização do exercício 5

---
+++ ex5.js
@@ -0,0 +1,23 @@
+
+let cores = ['azul', 'verde', 'amarelo'];
+cores.push('vermelho');
+console.log(cores); 
+cores.shift();
+console.log(cores);
+
+
+let livro = { 
+    titulo: "Dom Casmurro", 
+      autor: "Machado de Assis", 
+      ano: 1899 };  
+
+livro.genero = "Romance";
+
+console.log(`titulo: ${livro.titulo}`);
+console.log(`autor:', ${livro.autor}`);
+console.log(`ano:', ${livro.ano}`);
+console.log(`genero:', ${livro.genero}`);
+
+
+console.log(Object.keys(livro));
+console.log(Object.values(livro));
