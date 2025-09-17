89eb0b28fb495d4dc2dcb6c709208576df9f88cc
amandapietra <amandapietra1595@gmail.com>
2025-09-16 21:52:38 -0300

    exercício 5

--- ex5.js
+++ ex5.js
@@ -1,30 +1,33 @@
 // 1. Crie um array chamado `cores` com pelo menos 3 cores.
 const cores = ["azul", "vermelho", "verde"];
-console.log(cores)
 
 // 2. Adicione uma nova cor ao final do array.
-cores.push("amarelo");
+cores.push("preto");
+cores.unshift("branco");
 
 // 3. Remova a primeira cor do array.
-cores.shift();
+cores.shift()
+cores.pop()
 
 // 4. Imprima o array resultante.
 console.log("Array de cores:", cores);
 
 // 5. Crie um objeto `livro` com as propriedades `titulo`, `autor` e `anoPublicacao`.
-let livro = {
-    titulo: "Dom Casmurro",
-    autor: "Machado de Assis",
-    anoPublicacao: 1899
+const livro = {
+    titulo: "Por lugares incríveis",
+    autor: "Jennifer Niven",
+    anoPublicacao: 2015
 };
 
 // 6. Adicione uma nova propriedade `genero` ao objeto `livro`.
 livro.genero = "Romance";
+livro["numeroDePaginas"] = 355
+console.log(livro)
 
 // 7. Imprima o `titulo` e o `autor` do livro.
-console.log("Título:", livro.titulo);
-console.log("Autor:", livro.autor);
+console.log(livro.titulo)
+console.log(livro.autor)
 
 // 8. Use `Object.keys()` e `Object.values()` para imprimir as chaves e os valores do objeto `livro`.
-console.log("Chaves do objeto livro:", Object.keys(livro));
-console.log("Valores do objeto livro:", Object.values(livro));
+console.log(Object.keys(livro))
+console.log(Object.values(livro))
